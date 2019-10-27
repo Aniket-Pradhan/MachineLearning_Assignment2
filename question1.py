85a086c8ff5d9f04ea1722d66618be45dda32777
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-10-27 13:41:13 +0530

    Modified code to use linear SVM

--- question1.py
+++ question1.py
@@ -1,32 +1,54 @@
 import time
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 # get_ipython().run_line_magic('ma  tplotlib', 'inline')
-from sklearn import svm
 from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC
 
 from scripts import get_data
 
+def savePickle(self, filename, data):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-data = get_data.data()
-data.load_cifar_labels()
-data.load_cifar_train(0)
-data.load_cifar_test()
-# print(data.train_data[0].shape)
-# plt.imshow(x[0])
+def loadPickle(self, filename):
+    with open(filename, 'rb') as handle:
+        b = pickle.load(handle)
+    return b
 
-def svc_param_selection(X, y, nfolds):
-    Cs = [0.001, 0.01, 0.1, 1, 10]
-    gammas = [0.001, 0.01, 0.1, 1]
-    param_grid = {'C': Cs, 'gamma' : gammas}
-    grid_search = GridSearchCV(svm.SVC(kernel='rbf'), param_grid, cv=nfolds)
-    grid_search.fit(X, y)
-    grid_search.best_params_
+def svc_param_selection(X, Y, numFolds):
+    Cs = [0.001, 0.01, 0.1]
+    param_grid = {'C': Cs}
+    grid_search = GridSearchCV(LinearSVC(random_state=0, tol=1e-5), param_grid, cv=nfolds, verbose=10)
+    grid_search.fit(X, Y)
+    # print(grid_search.best_params_)
     return grid_search.best_params_
 
-print("in")
-start_time = time.time()
-print(svc_param_selection(data.train_data, data.train_labels, 5))
-print("out")
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--preprocess", required=False, help="Just preprocess the train and test data.", action="store_true")
+    ap.add_argument("-lt", "--loadtraintestdata", required=False, help="Use cached/stored train test data.", action="store_true")
+    ap.add_argument("-n", "--num-folds", required=False, default=3, help="Number of folds to find the best param.")
+    args = vars(ap.parse_args())
+
+    preprocess = args["preprocess"]
+    loadtraintestdata = args["loadtraintestdata"]
+    numFolds = args["numFolds"]
+
+    data = get_data.data(loadtraintestdata)
+    data.load_cifar_labels()
+    data.load_cifar_train(0)
+    data.load_cifar_test()
+
+    if preprocess:
+        print("preprocessed and stored the dataset")
+        exit(0)
+    if loadtraintestdata:
+        print("loaded train_test data")
+
+    print("Finding best params for the linear SVM")
+    start_time = time.time()
+    best_params = svc_param_selection(data.train_data, data.train_labels, numFolds)
+    print("Time taken: %s seconds ---" % (time.time() - start_time))
--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,8 +1,19 @@
 import pickle
 import numpy as np
 from pathlib import Path
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
 
 class data:
+
+    def savePickle(self, filename, data):
+        with open(filename, 'wb') as file:
+            pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    def loadPickle(self, filename):
+        with open(filename, 'rb') as handle:
+            b = pickle.load(handle)
+        return b
 
     def unpickle(self, file):
         with open(file, 'rb') as fo:
@@ -10,30 +21,87 @@ class data:
         return _dict
     
     def load_cifar_labels(self):
+        labelfile = self.cifar_dataset + "/labelingdata_processed"
+        # load labeling data
+        if self.load:
+            label_data = self.loadPickle(labelfile)
+            self.labels = label_data["names"]
+            self.num_train_elements = label_data["num"]
+            return
+
         file_name = self.cifar_dataset + "/batches.meta"
         labels = self.unpickle(file_name)
         self.labels = labels["label_names"]
         self.num_train_elements = labels["num_cases_per_batch"]
+
+        label_data = {}
+        label_data["names"] = self.labels
+        label_data["num"] = self.num_train_elements
+        self.savePickle(labelfile, label_data)
     
     def load_cifar_test(self):
+        testfile = self.cifar_dataset + "/testingdata_processed"
+        # load testing data
+        if self.load:
+            test_data = self.loadPickle(testfile)
+            self.test_data = test_data["data"]
+            self.test_labels = test_data["labels"]
+            return
+
         file_name = self.cifar_dataset + "/test_batch"
         test = self.unpickle(file_name)
-        self.test_data = test["data"]
-        self.test_data = self.test_data.reshape((len(self.test_data), 3, 32, 32)).transpose(0, 2, 3, 1)
+        test_data = test["data"]
+        test_data = test_data.reshape((len(test_data), 3, 32, 32)).transpose(0, 2, 3, 1)
+        test_data = test_data.astype('float32')
+        test_data = test_data / 255.0
+        self.test_data = []
+        ## convert to grayscale
+        for im_ind in range(len(test_data)):
+            self.test_data.append(rgb2gray(test_data[im_ind]))
+        self.test_data = np.array(self.test_data)
+
         self.test_data = np.reshape(self.test_data, (self.test_data.shape[0], -1))
         self.test_labels = test["labels"]
+
+        test_data = {}
+        test_data["data"] = self.test_data
+        test_data["labels"] = self.test_labels
+        self.savePickle(testfile, test_data)
     
     def load_cifar_train(self, ind):
+        trainfile = self.cifar_dataset + "/trainingdata_processed"
         # load training data
-        train_data_names = ["/data_batch_1", "/data_batch_2", "/data_batch_3", "/data_batch_4", "/data_batch_5"]
-        file_name = self.cifar_dataset + train_data_names[ind]
-        train = self.unpickle(file_name)
-        self.train_data = train["data"]
-        self.train_data = self.train_data.reshape((len(self.train_data), 3, 32, 32)).transpose(0, 2, 3, 1)
-        self.train_data = np.reshape(self.train_data, (self.train_data.shape[0], -1))
-        self.train_labels = train["labels"]
+        if self.load:
+            train_data = self.loadPickle(trainfile)
+            self.train_data = train_data["data"]
+            self.train_labels = train_data["labels"]
+            return
 
-    def __init__(self):
+        train_data_names = ["/data_batch_1", "/data_batch_2", "/data_batch_3", "/data_batch_4", "/data_batch_5"]
+        self.train_data = []
+        self.train_labels = []
+        for name in train_data_names:
+            file_name = self.cifar_dataset + name
+            train = self.unpickle(file_name)
+            train_data = train["data"]
+            train_data = train_data.reshape((len(train_data), 3, 32, 32)).transpose(0, 2, 3, 1)
+            train_data = train_data.astype('float32')
+            train_data = train_data / 255.0
+            ## convert to grayscale
+            for im_ind in range(len(train_data)):
+                self.train_data.append(rgb2gray(train_data[im_ind]))
+            self.train_labels.extend(train["labels"])
+        
+        self.train_data = np.array(self.train_data)
+        self.train_data = np.reshape(self.train_data, (self.train_data.shape[0], -1))
+        
+        train_data = {}
+        train_data["data"] = self.train_data
+        train_data["labels"] = self.train_labels
+        self.savePickle(trainfile, train_data)
+
+    def __init__(self, load):
         self.root = str(Path(__file__).parent.parent)
         self.dataset_directory = self.root + "/datasets"
         self.cifar_dataset = self.dataset_directory + "/cifar"
+        self.load = load
