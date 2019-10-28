1634c400e59ee7aa5c9c922bbf1de88cad553365
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-10-28 18:26:31 +0530

    Implemented ROC Curves

--- nb.py
+++ nb.py
@@ -12,6 +12,7 @@ from sklearn import tree
 from sklearn import preprocessing
 from sklearn.metrics import f1_score
 from sklearn.datasets import load_wine
+from sklearn.metrics import roc_curve, auc
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
@@ -62,6 +63,7 @@ if __name__ == "__main__":
     ldt = args["loaddectree"]
 
     raw_data = load_wine()
+    n_classes = 3
 
     wine_data = raw_data["data"]
     wine_index = [i+1 for i in range(len(wine_data))]
@@ -80,6 +82,16 @@ if __name__ == "__main__":
     train_Y = train_Y.values
     test_X = test_X.values
     test_Y = test_Y.values
+
+    y_test_roc = [0] * n_classes
+    for _ in range(n_classes):
+        y_test_roc[_] = []
+    for i in test_Y:
+        y_test_roc[i[0]].append(1)
+        for j in range(n_classes):
+            if j != i[0]:
+                y_test_roc[j].append(0)
+    y_test_roc = np.array(y_test_roc)
 
     if plotdata:
         print("Plotting")
@@ -108,6 +120,42 @@ if __name__ == "__main__":
         # Calculate metrics globally by counting the total true positives, false negatives and false positives.
         print("Training F1-Score", f1_score(train_Y, train_Y_pred, average='micro') )
         print("Testing F1-Score", f1_score(test_Y, test_Y_pred, average='micro') )
+
+        # y_score = gauss_nb_model.predict(test_X)
+        y_pred_roc = [0] * n_classes
+        for _ in range(n_classes):
+            y_pred_roc[_] = []
+        for i in test_Y_pred:
+            y_pred_roc[i].append(1)
+            for j in range(n_classes):
+                if j != i:
+                    y_pred_roc[j].append(0)
+        y_pred_roc = np.array(y_pred_roc)
+        
+        # print(y_test_roc[0], y_pred_roc[0])
+        # exit()
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_test_roc[:, i], y_pred_roc[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Compute micro-average ROC curve and ROC area
+        # fpr["micro"], tpr["micro"], _ = roc_curve(y_test_roc.ravel(), y_pred_roc.ravel())
+        # roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+        plt.figure()
+        lw = 1
+        colors = ["red", "blue", "darkorange"]
+        for i in range(n_classes):
+            plt.plot(fpr[i], tpr[i], color=colors[i],
+                    lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[i])
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.show()
     
     if dt or ldt:
         start_time = time.time()
@@ -129,3 +177,39 @@ if __name__ == "__main__":
         # Calculate metrics globally by counting the total true positives, false negatives and false positives.
         print("Training F1-Score", f1_score(train_Y, train_Y_pred, average='micro') )
         print("Testing F1-Score", f1_score(test_Y, test_Y_pred, average='micro') )
+        # Dec tree
+        tree.plot_tree(dectree_model)
+        plt.show()
+
+        y_pred_roc = [0] * n_classes
+        for _ in range(n_classes):
+            y_pred_roc[_] = []
+        for i in test_Y_pred:
+            y_pred_roc[i].append(1)
+            for j in range(n_classes):
+                if j != i:
+                    y_pred_roc[j].append(0)
+        y_pred_roc = np.array(y_pred_roc)
+        
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(n_classes):
+            fpr[i], tpr[i], _ = roc_curve(y_test_roc[:, i], y_pred_roc[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Compute micro-average ROC curve and ROC area
+        # fpr["micro"], tpr["micro"], _ = roc_curve(y_test_roc.ravel(), y_pred_roc.ravel())
+        # roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+        plt.figure()
+        lw = 1
+        colors = ["red", "blue", "darkorange"]
+        for i in range(n_classes):
+            plt.plot(fpr[i], tpr[i], color=colors[i],
+                    lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[i])
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.show()
